1550f74bec3114b60384c7c4e1f32850eb095656
Paul13G <119010010@link.cuhk.edu.cn>
2022-04-01 00:58:17 +0800

    modify homepage

--- client/src/views/ActivityCreationView.vue
+++ client/src/views/ActivityCreationView.vue
@@ -1,7 +1,43 @@
 <template>
   <div class="home">
-    <h1>This is the Activity Creation page </h1>
+    <div class="outer">
+      <div>
+        <div class="head"><h1>post a new event</h1></div>
+        <div class="headB" align="center"><button id="postAct">Post</button></div>
+      </div>
+      <div>
+        <div style="width:20%; float:left"><h3>Activity Type</h3></div>
+        <div style="width:80%; float:left; align:center">
+          <button class="singleSelect"></button><span>Sports</span>
+          <button class="singleSelect"></button><span>Meals</span>
+          <button class="singleSelect"></button><span>Travel</span>
+          <button class="singleSelect"></button><span>Shop online</span>
+          <button class="singleSelect"></button><span>Carpool</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script></script>
+
+<style>
+.outer{
+  position: fixed; left: 10%; width: 80%; height: 100%;
+}
+
+.head{
+  float: left; width: 80%; height: 100px; margin: 10px 0px 10px 0px;
+}
+
+.headB{
+  float: left;
+  width: 20%;
+  height: 100px;
+  margin: 10px 0px 10px 0px;
+}
+
+.singleSelect{
+  height: 20px; width: 20px; margin: 0px 5px 0px 5px;
+}
+</style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,7 +1,162 @@
 <template>
   <div class="home">
-    <h1>This is the Home page</h1>
+    <table class="showTable" cellspacing="0" cellpadding="0" align="center">
+      <tr height="100">
+        <td id="swip" align="center" colspan="3" border="1">
+          <swiper options="swiperOption" class = 'swiper-box'>
+            <swiper-slide height="50" align="center">
+              <div >content</div>
+            </swiper-slide>
+          </swiper>
+        </td>
+        <td id="create" align="center">
+          <button id = "createNew" @click="switchTo('/activityCreation')">
+          + Post a new event</button>
+        </td>
+      </tr>
+      <tr height="50">
+        <td align="center" width="30%">search bar</td>
+        <td align="center" width="20%">
+          <div class="rightSep">
+            <form>
+              <select name="orders">
+              <option value="Most Recent">Most Recent</option>
+              <option value="Almost Full" selected="selected">Almost Full</option>
+              </select>
+              <input type="submit" value="order the activity">
+            </form>
+          </div>
+        </td>
+        <td align="center" width="20%">
+          <div class="rightSep">
+            <datepicker>
+            </datepicker>
+          </div>
+        </td>
+        <td align="center" width="30%">number of member</td>
+      </tr>
+    </table>
+
+    <div class="actSquare">
+      <div class="actCard">activity</div>
+      <div class="actCard">activity</div>
+      <div class="actCard">activity</div>
+      <div class="actCard">activity</div>
+      <div class="actCard">activity</div>
+      <div class="actCard">activity</div>
+      <div class="actCard">activity</div>
+      <div class="actCard">activity</div>
+      <div class="actCard">activity</div>
+      <div class="actCard">activity</div>
+      <div class="actCard">activity</div>
+      <div class="actCard">activity</div>
+      <div class="pageList">
+        <div class="pageIndex">1</div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script></script>
+<script>
+import { swiper, swiperSlide } from 'vue-awesome-swiper';
+import Datepicker from 'vuejs3-datepicker';
+
+export default {
+  components: {
+    swiper,
+    swiperSlide,
+    Datepicker,
+  },
+
+  data() {
+    return {
+      swiperOption: {
+        autoplay: 3000,
+        pagination: '.swiper-pagination',
+        autoplayDisableOnInteraction: true,
+        loop: true,
+      },
+      swipers: [],
+    };
+  },
+
+  methods: {
+    switchTo(path) {
+      this.$router.replace(path);
+    },
+  },
+
+};
+</script>
+
+<style>
+#swip {
+  border-style: solid;
+  border-width: 0px 1px 1px 0px;
+}
+
+#create {
+  border-style: solid;
+  border-width: 0px 0px 1px 0px;
+}
+
+#createNew {
+  text-align:center;
+  background-color: white;
+  border-radius: 10px;
+  height:45px;
+  width: 200px;
+
+}
+
+.rightSep{
+  margin: 5px;
+  border-style: solid;
+  border-width: 0px 1px 0px 0px;
+}
+
+.showTable{
+  width: 100%;
+  border-style: solid;
+  border-width: 1px 0px 1px 0px;
+  border-color: black;
+}
+
+.actSquare{
+  padding-left: 4%;
+  padding-right: 4%;
+  padding-top: 1%;
+  align-content: center;
+}
+
+.actCard{
+  float: left;
+  width: 275px;
+  height: 125px;
+  border-radius: 10px;
+  align-content: center;
+  border-style:solid;
+  border-width:2px;
+  margin: 15px;
+  box-shadow: 5px 2px 6px#000;
+}
+
+.pageList{
+  position: fixed;
+  width: 60%;
+  bottom: 10px;
+  left: 20%;
+  right: 20%
+}
+
+.pageIndex{
+  width: 30px;
+  height: 30px;
+  border-style: solid;
+  border-width: 1px;
+  border-radius: 5px;
+  text-align: center;
+  color: slateblue;
+}
+
+</style>
